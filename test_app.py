1bbbccd1b897544e968819826669b74b5051c3cd
premfeb92 <premfeb92@gmail.com>
2024-05-15 16:32:16 +0530

    created falsk app with all requirements

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert b'Hello, World!' in response.data
